cdd03afde3ea14d914b3c08cc39317558571b824
velimirT <velimir.tchatchevsky@gmail.com>
2018-11-04 11:46:09 -0500

    Updates

---
+++ precache-manifest.d8e28c9d10f419e63c171e27a0be57a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "velimirT.github.io/assignment15/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "546531d2c4b31e921889",
+    "url": "velimirT.github.io/assignment15/static/js/runtime~main.546531d2.js"
+  },
+  {
+    "revision": "8ce614999b488193aadc",
+    "url": "velimirT.github.io/assignment15/static/js/main.8ce61499.chunk.js"
+  },
+  {
+    "revision": "740cc6a095238bc45cd1",
+    "url": "velimirT.github.io/assignment15/static/js/1.740cc6a0.chunk.js"
+  },
+  {
+    "revision": "8ce614999b488193aadc",
+    "url": "velimirT.github.io/assignment15/static/css/main.788aac0d.chunk.css"
+  },
+  {
+    "revision": "370659d5ea251fc1b77d062e81983824",
+    "url": "velimirT.github.io/assignment15/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "velimirT.github.io/assignment15/precache-manifest.d8e28c9d10f419e63c171e27a0be57a8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("velimirT.github.io/assignment15/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
